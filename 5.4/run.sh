606110b16fa62b4b945700ef1baa54bf37fdd71e
Kevin Vandenborne <kevin.vandenborne@gmail.com>
2016-07-20 14:57:13 +0200

    Attempt at fixing occasional "httpd (pid 11) already running" error

--- 5.4/run.sh
+++ 5.4/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid && \
 sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/apache2/sites-available/default && \
 sed -i "s|ENVIRONMENT_VAR|$ENVIRONMENT|g" /etc/apache2/sites-available/default && \
 /usr/sbin/apache2ctl -D FOREGROUND
