5c2364e7e70870e0f7309269185cf91ee94c4fa1
Joeri Verdeyen <info@jverdeyen.be>
2016-11-17 09:14:20 +0100

    PHP5.3: Using Ubuntu 12.04 as image

--- 5.3/run.sh
+++ 5.3/run.sh
@@ -4,4 +4,6 @@
 rm -f /var/run/apache2/apache2.pid && \
 sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/apache2/sites-available/default && \
 sed -i "s|ENVIRONMENT_VAR|$ENVIRONMENT|g" /etc/apache2/sites-available/default && \
+sed -i "s|PHP_ERROR_REPORTING_VAR|$PHP_ERROR_REPORTING|g" /etc/php5/apache2/conf.d/php.ini && \
+sed -i "s|PHP_ERROR_REPORTING_VAR|$PHP_ERROR_REPORTING|g" /etc/php5/cli/conf.d/php.ini && \
 /usr/sbin/apache2ctl -D FOREGROUND
